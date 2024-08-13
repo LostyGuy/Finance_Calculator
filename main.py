3c894f122961e9246a6c6b4370ecd203cb9b3ead
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2024-08-13 21:47:30 +0200

    Tkinter input validation
    Entry widgets have been added with working placeholders. Input from entry's is now checked wherever it is a non float convertable data.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk #styles
 import tkinter as tk
+from tkinter import messagebox
 
 def calulate_finances(monthly_income: float, tax_rate: float, currency: str) -> None:
     
@@ -13,7 +14,6 @@ def calulate_finances(monthly_income: float, tax_rate: float, currency: str) -> 
 #   |,.2f| Will round the number to two places after point
     global text1,text2,text3,text4,text5,text5,text6,text7
 
-    print("-" * 20)
     text1 = (f'Montly income: {currency} {monthly_income: ,.2f}')
     text2 = (f'Tax rate: {tax_rate:,.0f}%')
     text3 = (f'Monthly tax: {currency} {monthly_tax:,.2f}')
@@ -21,21 +21,26 @@ def calulate_finances(monthly_income: float, tax_rate: float, currency: str) -> 
     text5 = (f'Yearly salary: {currency} {yearly_salary:,.2f}')
     text6 = (f'Yearly tax to pay: {currency} {yearly_tax:,.2f}')
     text7 = (f'Yearly net income: {currency} {yearly_net_income:,.2f}')
-    print("-" * 20)
+    print('Output has been sent')
     
 
 def main() -> None:
 
+    try:
+        monthly_income = float(monthly_income1.get())
+    except:
+       messagebox.showerror('Data type error', 'Wrong data given!')
+       pass
+
+    try:
+        tax_rate = float(tax_rate1.get())
+    except:
+       messagebox.showerror('Data type error', 'Wrong data given!')
+       return None
+
     calc_window = Toplevel(main_window)
     calc_window.geometry('520x400')
     calc_window.resizable(False,False)
-
-    
-
-    #monthly_income: float = float(input('Enter your monthly salary: '))
-    #tax_rate: float = float(input('Enter your tax rate (%): '))
-
-    #main_window.destroy()
     
     calulate_finances(monthly_income, tax_rate, currency = "$")
 
@@ -62,7 +67,7 @@ def main() -> None:
 #if __name__ ==  '__main__':
 #    main()
 
-#   Begining of TKinter Window
+#   Begining of TKinter Window  #
 
 main_window = tk.Tk()
 main_window.title('Financial Calculator')
@@ -76,22 +81,44 @@ frame2.pack(fill='x')
 frame3 = Frame(main_window,relief=RAISED, name='footer')
 frame3.pack(fill='x')
 
+def on_entry_click1(event):
+   if monthly_income1.get() == 'Enter your monthly salary':
+      monthly_income1.delete(0, tk.END)
+      monthly_income1.configure(foreground="black")
+
+def on_focus_out1(event):
+   if monthly_income1.get() == "":
+      monthly_income1.insert(0, 'Enter your monthly salary')
+      monthly_income1.configure(foreground="gray")
+
+def on_entry_click2(event):
+   if tax_rate1.get() == 'Enter your tax rate':
+      tax_rate1.delete(0, tk.END)
+      tax_rate1.configure(foreground="black")
+
+def on_focus_out2(event):
+   if tax_rate1.get() == "":
+      tax_rate1.insert(0, 'Enter your tax rate')
+      tax_rate1.configure(foreground="gray")
+
 title = Label(frame1, text='Welcome to the Calculator', bg='lightblue')
 title.pack(fill='x')
 title.config(font=('Font', 25))
 
-monthly_income = Entry(frame2,)
-monthly_income.insert(0, 'Enter your monthly salary')
-monthly_income.pack()
+monthly_income1 = Entry(frame2)
+monthly_income1.insert(0, 'Enter your monthly salary')
+monthly_income1.bind("<FocusIn>", on_entry_click1)
+monthly_income1.bind("<FocusOut>", on_focus_out1)
+monthly_income1.pack()
 
-tax_rate = Entry(frame2)
-tax_rate.insert(0, 'Enter your tax rate')
-tax_rate.pack()
-
+tax_rate1 = Entry(frame2)
+tax_rate1.insert(0, 'Enter your tax rate')
+tax_rate1.bind("<FocusIn>", on_entry_click2)
+tax_rate1.bind("<FocusOut>", on_focus_out2)
+tax_rate1.pack()
 
 calc_start = Button(frame2, text='Take me to the Calculator',  command=main)
 calc_start.pack()
 calc_start.config(font=('font', 10))
-
 
 main_window.mainloop() #   creates visual window
